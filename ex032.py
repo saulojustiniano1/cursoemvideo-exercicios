c8e9dd3edd016d8506f2ad2d02a14301051f09a5
saulojustiniano1 <saulojustiniano1@gmail.com>
2022-10-10 19:39:46 -0300

    📦create: python-exercicios

--- ex032.py
+++ ex032.py
@@ -8,5 +8,3 @@ if ano % 4 == 0 and ano % 400 == 0 or ano % 4 == 0 and ano % 100 != 0:
   print(f'O ano {ano} é BISSEXTO')
 else:
   print(f'O ano {ano} NÃO É BISSEXTO')
-
-
---
+++ ex033b.py
@@ -0,0 +1,20 @@
+valor_1 = int(input('Primeiro valor: '))
+valor_2 = int(input('Segundo valor: '))
+valor_3 = int(input('Terceiro valor: '))
+
+# Verificando quem é o menor
+menor = valor_1
+if valor_2 < valor_1 and valor_2 < valor_3:
+  menor = valor_2
+if valor_3 < valor_1 and valor_3 < valor_2:
+  menor = valor_3
+
+# Vericando quem é o maior
+maior = valor_1
+if valor_2 > valor_1 and valor_2 > valor_3:
+  maior = valor_2
+if valor_3 > valor_1 and valor_3 > valor_2:
+  maior = valor_3
+
+print(f'O menor valor digitado foi {menor}')
+print(f'O maior valor digitado foi {maior}')
